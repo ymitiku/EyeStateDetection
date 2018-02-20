8de6e10824d0e6e6153e6af9fe80f13e2d03c678
Mitiku <se.mitiku.yohannes@gmail.com>
2018-02-20 12:49:01 +0300

    Initail commit

---
+++ demo/__init__.py
@@ -0,0 +1,9 @@
+def webcam_demo(model_path):
+    pass
+def video_demo(model_path,video_path):
+    pass
+def image_demo(model_path,image_path):
+    pass
+
+def process_video(model_path):
+    pass
